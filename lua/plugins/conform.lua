37886819f650f32f44e8fe0ce288b7074e83353a
jacobXPS <Jawar19@student.sdu.dk>
2025-11-12 10:39:22 +0100

    Fix markdown linting
    Add markdown lint fix and cspell to lint.
    Markdown Toc also implemented

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				json = { "prettierd", "prettier" },
-				markdown = { "prettierd", "prettier" },
+				markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
 				css = { "prettierd", "prettier" },
 				scss = { "prettierd", "prettier" },
 				bash = { "beautysh" },
@@ -29,9 +29,26 @@ return {
 				["clang-format"] = {
 					command = "/usr/bin/clang-format",
 				},
+				["markdown-toc"] = {
+					condition = function(_, ctx)
+						for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+							if line:find("<!%-%- toc %-%->") then
+								return true
+							end
+						end
+					end,
+				},
+				["markdownlint-cli2"] = {
+					condition = function(_, ctx)
+						local diag = vim.tbl_filter(function(d)
+							return d.source == "markdownlint"
+						end, vim.diagnostic.get(ctx.buf))
+						return #diag > 0
+					end,
+				},
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 3000,
 				lsp_fallback = true,
 			},
 		})
